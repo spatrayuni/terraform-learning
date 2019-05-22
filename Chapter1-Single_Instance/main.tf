62e9a640123205be7a7de531ef025f7cde41eaa1
Santhosh Deepu Patrayuni <santhoshdeepu_patrayuni@intuit.com>
2019-05-22 12:19:12 +0530

    Changed the code to Bastion instance

--- Chapter1-Single_Instance/main.tf
+++ Chapter1-Single_Instance/main.tf
@@ -3,13 +3,15 @@ provider "aws" {
   profile = "learning"
 }
 
-resource "aws_instance" "Deepu-Ec2" {
+resource "aws_instance" "Deepu-Bastion" {
   ami = "ami-0d53ab604830478ff"
   instance_type = "t2.micro"
   key_name = "spatrayuni4"
-  subnet_id = "subnet-0585099563414ac77"
   vpc_security_group_ids = ["sg-01cd8fa17d41d4481"]
+  subnet_id = "subnet-09b74995027eab3a3"
+  associate_public_ip_address = true
+
   tags = {
-    Name = "Deepu-Ec2"
+    Name = "Deepu-Bastion"
   }
 }
